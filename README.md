55c17bd503e201116c45b24cf6c6fddf1993690d
Rodrigo A. Madureira <rodrigomadu85@gmail.com>
2026-03-16 08:52:13 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# MRP Real Cost
---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "MRP Real Production Cost",
+    "version": "16.0.1.0.0",
+    "depends": [
+        "mrp",
+        "stock",
+        "product"
+    ],
+    "author": "Rodrigo A. Madureira",
+    "category": "Manufacturing",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/mrp_cost_report_views.xml",
+        "views/mrp_cost_menu.xml",
+        "report/mrp_cost_report_template.xml",
+    ],
+    "installable": True,
+}
