ded7ed7ac5dea081561897572c504ffa2e79e266
rodrigo <rodrigomadu85@gmail.com>
2026-03-16 11:09:07 -0300

    Atualizado manifest

--- __manifest__.py
+++ __manifest__.py
@@ -10,9 +10,10 @@
     "category": "Manufacturing",
     "data": [
         "security/ir.model.access.csv",
-        "views/mrp_cost_menu.xml",
         "views/mrp_cost_report_views.xml",
         "report/mrp_cost_report_template.xml",
+        "views/mrp_cost_menu.xml",
+
     ],
     "installable": True,
 }
